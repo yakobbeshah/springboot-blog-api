abf76bf2a3fc7fa4b105a3e9dd0004ef9f58e9dc
yakobbeshah <beshayakob51@gmail.com>
2023-07-13 10:55:04 +0300

    Initial commit

---
+++ src/test/java/com/springbootblogapi/SpringbootBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootblogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
